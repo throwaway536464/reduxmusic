ea40a00ff7fd0d15158286d5f3af691bc34ac4fb
throwaway536464 <65782686+throwaway536464@users.noreply.github.com>
2020-05-22 10:58:44 -0700

    Update musicCard.tsx

--- src/modules/MusicSearch/containers/musicCard.tsx
+++ src/modules/MusicSearch/containers/musicCard.tsx
@@ -1,28 +1,25 @@
 import * as React from "react";
 import { Card } from "@uifabric/react-cards";
 import { Text } from "@fluentui/react";
-import { useSelector } from "react-redux";
-import musicSelectors from "../musicSelectors";
 
 type TrackCardProps = {
     trackId: string;
 }
 
 export const MovieCard = (props: TrackCardProps) => {
-    const track:ITrack;
     return (
         <Card className="music-card">
             <Card.Item>
                 <img 
                  alt={"movie"}
                  className="card-image" 
-                 src={track.artworkUrl100} />
-                <Text block styles={{root: {padding: "3px", fontSize: "20px", color: "darkgreen"}}}>{track.trackName}</Text>
-                <Text block styles={{root: {padding: "3px"}}}>{track.artistName}</Text>
+                />
+                <Text block styles={{root: {padding: "3px", fontSize: "20px", color: "darkgreen"}}}></Text>
+                <Text block styles={{root: {padding: "3px"}}}></Text>
             </Card.Item>
             <Card.Item>
-                <Text block styles={{root: {padding: "3px"}}}>{track.collectionName}</Text>
-                <Text block styles={{root: {padding: "3px"}}}>{new Date(track.releaseDate).toLocaleDateString()}</Text>
+                <Text block styles={{root: {padding: "3px"}}}></Text>
+                <Text block styles={{root: {padding: "3px"}}}></Text>
             </Card.Item>
         </Card>
     );
