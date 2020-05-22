d810a2861b56822c31068fdcc34c4285a6166bbf
throwaway536464 <65782686+throwaway536464@users.noreply.github.com>
2020-05-22 11:02:46 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,14 @@ import { Provider } from 'react-redux';
 import "./App.scss";
 import { Header } from './header/header';
 import { MovieSearch } from './modules/MusicSearch/containers/musicSearchPage';
-import store from './store/configureStore';
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <Header />
-        <MovieSearch />
-      </Provider>
+      <Header />
+      <MovieSearch />
     </div>
   );
 }
 
-
 export default App;
-
