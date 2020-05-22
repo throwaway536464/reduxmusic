72f8974a0f1c3622e4f17294fbada60e1f9c00a1
throwaway536464 <65782686+throwaway536464@users.noreply.github.com>
2020-05-22 11:01:03 -0700

    Update musicSearchSagas.ts

--- src/modules/MusicSearch/musicSearchSagas.ts
+++ src/modules/MusicSearch/musicSearchSagas.ts
@@ -1,21 +1,21 @@
 import { PayloadAction } from "@reduxjs/toolkit";
 import { call, takeLatest, all, put } from "redux-saga/effects";
 import musicSearchApi from "./tracks.api";
-import { searchMusicStart, searchMusicSuccess, searchMusicFailed } from "./musicSearchSlice";
+// import { searchMusicStart, searchMusicSuccess, searchMusicFailed } from "./musicSearchSlice";
 
 function* search(action: PayloadAction<string>) {
     try {
         const response = yield call(musicSearchApi.searchForTracks, action.payload);
-        yield put(searchMusicSuccess(response));
+        //yield put(searchMusicSuccess(response));
     }
     catch(err) {
         console.log(err);
-        yield put(searchMusicFailed(err.toString()))
+        //yield put(searchMusicFailed(err.toString()))
     }
 }
 
 export default function* watchMusicSearchSagas() {
     yield all([
-        yield takeLatest(searchMusicStart.toString(), search)
+        //yield takeLatest(searchMusicStart.toString(), search)
     ]);
 }
