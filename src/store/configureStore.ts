84dcaba4ccbc15ec43333210f26cfad7e1e4cbd2
throwaway536464 <65782686+throwaway536464@users.noreply.github.com>
2020-05-22 11:01:57 -0700

    Update configureStore.ts

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,22 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from "@reduxjs/toolkit";
-import searcMusicReducers from "../modules/MusicSearch/musicSearchSlice";
-import musicSearchSagas from "../modules/MusicSearch/musicSearchSagas";
-
 import createSagaMiddleware from "redux-saga";
+import musicSearchSagas from "../modules/MusicSearch/musicSearchSagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const rootReducer = combineReducers({
-    musicSearch: searcMusicReducers
-});
-
-const store = createStore(
-    rootReducer,
-    applyMiddleware(sagaMiddleware)
-);
-
 sagaMiddleware.run(musicSearchSagas);
-
-export type MusicAppState = ReturnType<typeof rootReducer>
-
-export default store;
